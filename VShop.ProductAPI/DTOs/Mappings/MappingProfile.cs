415bfd94d414b3f533f83e49e8dba35e60549255
Bruna <brunasoaresoliv@outlook.com>
2023-04-24 16:51:02 -0300

    add DTO's e repository

---
+++ VShop.ProductAPI/DTOs/Mappings/MappingProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+
+namespace VShop.ProductAPI.DTOs.Mappings
+{
+    public class MappingProfile: Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<CategoryDTO, CategoryDTO>().ReverseMap();     
+            CreateMap<ProductDTO, ProductDTO>().ReverseMap();
+        }
+    }
+}
---
+++ VShop.ProductAPI/Repository/ICategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using VShop.ProductAPI.Models;
+
+namespace VShop.ProductAPI.Repository
+{
+    public interface ICategoryRepository
+    {
+        Task<IEnumerable<Category>> GetAll();
+        Task<IEnumerable<Category>> GetCategoiresProducts();
+        Task<Category> GetById(int id);
+        Task<Category> Create(Category category);
+        Task<Category> Update(Category category);
+        Task<Category> Delete(int id);
+    }
+}
